1be94d194c3ddbeb2cc464e5d5c9b1e99772ce4f
Conor Doyle <doyle7@hotmail.com>
2017-06-10 16:04:15 +0100

    Disable AirBnB's 'no-tabs' default setting so I can use tabs.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ var ReactDOM = require('react-dom');
 var App = require('./components/App');
 require('./index.css');
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app'),
-);
+ReactDOM.render(<App />, document.getElementById('app'));
+
+function test(num1, num2) {
+	return num1 + num2;
+}
+
+const result = test(2, 1);
+console.log(result);
